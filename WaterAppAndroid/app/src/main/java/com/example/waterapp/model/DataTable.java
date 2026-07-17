4d9eeb6d2fb573dd17aa7f6ca51caf1322e32674
xiaoshideguangnian <88666121+xiaoshideguangnian@users.noreply.github.com>
2026-07-17 17:16:35 +0800

    Delete WaterAppAndroid directory

--- WaterAppAndroid/app/src/main/java/com/example/waterapp/model/DataTable.java
+++
@@ -1,9 +0,0 @@
-package com.example.waterapp.model;
-
-import java.util.List;
-
-public class DataTable {
-    public List<String> headers;
-    public List<List<String>> rows;
-    public int totalRows;
-}
