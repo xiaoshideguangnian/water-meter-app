b5f3526af13229a7cfc0aec251ce898eb4e0a11f
xiaoshideguangnian <88666121+xiaoshideguangnian@users.noreply.github.com>
2026-07-17 17:17:44 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/waterapp/model/DataTable.java
@@ -0,0 +1,9 @@
+package com.example.waterapp.model;
+
+import java.util.List;
+
+public class DataTable {
+    public List<String> headers;
+    public List<List<String>> rows;
+    public int totalRows;
+}
